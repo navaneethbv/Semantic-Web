f8e92e5d53083d142b7fafb2dc7f596a44eeb906
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-03-25 22:22:03 -0500

    Update Lab1_1.java

--- Assignment-1/Semantic_Web_Lab_1/src/utd/sw/assignment1/Lab1_1.java
+++ Assignment-1/Semantic_Web_Lab_1/src/utd/sw/assignment1/Lab1_1.java
@@ -14,8 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
+ */ 
 package utd.sw.assignment1 ;
 
 import org.apache.jena.rdf.model.*;
