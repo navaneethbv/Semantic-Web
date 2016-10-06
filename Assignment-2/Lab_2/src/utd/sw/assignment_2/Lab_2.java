2817c90b50f4d82aca29d93a2cbae610d6e54666
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-10-06 15:45:10 -0500

    Update Lab_2.java

--- Assignment-2/Lab_2/src/utd/sw/assignment_2/Lab_2.java
+++ Assignment-2/Lab_2/src/utd/sw/assignment_2/Lab_2.java
@@ -3,7 +3,7 @@ import java.io.*;
  
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.ReadWrite;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Model; 
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
